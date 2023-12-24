a807cf2d00bd2ea1b45f829e012b7a70e7eed94e
holys <goholys@gmail.com>
2023-12-24 10:29:55 +0200

    .

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,9 @@ const config = {
   testMatch: ['**/src/**/*.test.ts'],
   preset: 'ts-jest',
   testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
 };
 
 export default config;
